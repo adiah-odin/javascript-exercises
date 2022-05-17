777acd14323b0f734c43a0feb7f4020e8e22afaa
nathanieladiah <nathanieladiah@gmail.com>
2022-05-17 11:15:22 -0400

    Solve Sum All

---
+++ 05_sumAll/pseudocode.md
@@ -0,0 +1,9 @@
+Take the two input parameters
+Check that they are both numbers
+Check that they are both > 0
+Determine the smaller number:
+	if num1 > num2 
+	smaller = num1 > num2 ? num2 : num 1;
+	larger = num 1 > num2 ? num1 : num2
+make a loop starting with i = smaller until <= larger.
+add each value to total
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+	if (typeof(num1) != 'number' || typeof(num2) != 'number') {
+		return 'ERROR';
+	}
+
+	if (num1 < 0 || num2 < 0) {
+		return 'ERROR';
+	}
+
+	let smaller;
+	let larger;
+	let total = 0;
+
+	if (num1 > num2) {
+		larger = num1;
+		smaller = num2;
+	} else if (num1 < num2) {
+		larger = num2;
+		smaller = num1;
+	} else {
+		return num1;
+	}
+
+	for (let i = smaller; i <= larger; i++) {
+		total += i;
+	}
+
+	return total;
 
 };
+
+let response = sumAll(1, 3);
 
 // Do not edit below this line
 module.exports = sumAll;
