2ae4eb1a22bdd7e7831fa9586e9c627ea53db7d9
nathanieladiah <nathanieladiah@gmail.com>
2022-05-17 09:12:21 -0400

    Solution to string reverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+	const contents = [...string];
+	// let contents = Array.from(string);
+	let reversedContents = [];
 
+	for (let i = 1; i <= contents.length; i++) {
+		let index = contents.length-i;
+		reversedContents.push(contents[index]);
+	}
+
+	return reversedContents.join('');
 };
+
+reverseString('test')
 
 // Do not edit below this line
 module.exports = reverseString;
