c969b990f9f68e3787a164d5f7054c27d6e3b673
nathanieladiah <nathanieladiah@gmail.com>
2022-05-17 07:46:26 -0400

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
+	let output = "";
 
+
+	if (count >= 0) {
+		for (i=0; i < count; i++) {
+			output += string;
+		}
+		return output;
+	} else {
+		return "ERROR";
+	}
 };
 
 // Do not edit below this line
