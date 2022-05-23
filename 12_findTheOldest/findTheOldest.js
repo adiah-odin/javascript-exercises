0d61239d88ea22c6a196157febfe48446f17a5ae
nathanieladiah <nathanieladiah@gmail.com>
2022-05-23 15:39:21 -0400

    Solve the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+	let sortedPeople = people.sort( (a, b) => {
+		const ageA = a.yearOfDeath ? a.yearOfDeath - a.yearOfBirth : (new Date).getFullYear() - a.yearOfBirth;
+		const ageB = b.yearOfDeath ? b.yearOfDeath - b.yearOfBirth : (new Date).getFullYear() - b.yearOfBirth;
 
+		return ageA > ageB ? -1 : 1;
+	})
+
+	return sortedPeople[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
