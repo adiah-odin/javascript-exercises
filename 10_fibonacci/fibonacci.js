a9f7fca3ea8b4ba9197984d6d66e901efd8e8c72
nathanieladiah <nathanieladiah@gmail.com>
2022-05-23 14:52:03 -0400

    Solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+	let n = parseInt(number);
+	if (n < 0 ) {
+		return 'OOPS';
+	}
+	if (n === 1) {
+		return 1;
+	} else if ( n === 0) {
+		return 0;
+	}
 
+	return fibonacci(n-1) + fibonacci(n-2);
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
