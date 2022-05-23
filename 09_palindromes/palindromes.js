3487c98dde6833a52c7fd94848c95d96c77f6230
nathanieladiah <nathanieladiah@gmail.com>
2022-05-23 13:43:20 -0400

    Complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	// get an array of only letters
+	const characters = string
+			.toLowerCase()
+			.split('')
+			.filter(character => /[a-z]/.test(character));
 
+	let length = characters.length;
+	let palindrome = true;
+
+	for (let i = 0; i < length / 2; i++) {
+		// console.log(characters[i], characters[length - (i + 1)])
+		let character1=characters[i];
+		let character2=characters[length - (i + 1)];
+		if (character1 != character2) {
+			palindrome = false;
+		}
+	}
+	return palindrome;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
