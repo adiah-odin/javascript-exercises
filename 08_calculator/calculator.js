98008d6c7b6f2cf8e16b7b431759b2d067cca954
nathanieladiah <nathanieladiah@gmail.com>
2022-05-23 11:47:32 -0400

    Complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  // base condition
+  if (number == 0) {
+    return 1;
+  }
+
+  return number * factorial(number - 1);
 };
 
 // Do not edit below this line
